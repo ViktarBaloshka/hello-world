a5cec2965e0ae490ab47c293f76c9164b84d9448
ViktarBaloshka <vbolosko@gmail.com>
2022-03-16 12:01:50 +0100

    Initial commit

---
+++ src/test/java/com/helloworld/helloworld/SpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
